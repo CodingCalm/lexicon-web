9ebbe382061841e338875f3acd07354a546066f9
Henrik <henrikekonomi@gmail.com>
2024-11-27 10:31:07 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using lexicon_web_dashboard.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents();
+
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://lexicon-api-argqa9bbcwdefwh4.northeurope-01.azurewebsites.net/")
+});
+
+builder.Services.AddScoped<StudentService>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>();
+
+app.Run();
---
+++ Services/StudentService.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class StudentService
+{
+    private readonly HttpClient _httpClient;
+
+    public StudentService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    // GET: api/Students
+    public async Task<List<StudentDTO>> GetStudentsAsync()
+    {
+        return await _httpClient.GetFromJsonAsync<List<StudentDTO>>("api/Students");
+    }
+
+    // GET: api/Students/{id}
+    public async Task<StudentDTO?> GetStudentByIdAsync(int id)
+    {
+        return await _httpClient.GetFromJsonAsync<StudentDTO>($"api/Students/{id}");
+    }
+
+    // POST: api/Students
+    public async Task<StudentDTO?> CreateStudentAsync(StudentDTO student)
+    {
+        var response = await _httpClient.PostAsJsonAsync("api/Students", student);
+
+        if (response.IsSuccessStatusCode)
+        {
+            return await response.Content.ReadFromJsonAsync<StudentDTO>();
+        }
+
+        return null; // Or handle error response
+    }
+
+    // PUT: api/Students/{id}
+    public async Task<bool> UpdateStudentAsync(int id, StudentDTO student)
+    {
+        var response = await _httpClient.PutAsJsonAsync($"api/Students/{id}", student);
+        return response.IsSuccessStatusCode;
+    }
+
+    // DELETE: api/Students/{id}
+    public async Task<bool> DeleteStudentAsync(int id)
+    {
+        var response = await _httpClient.DeleteAsync($"api/Students/{id}");
+        return response.IsSuccessStatusCode;
+    }
+}
+
